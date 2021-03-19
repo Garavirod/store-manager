dff3be71296ff87cbe624e8f647a52140440bf0c
Garavirod <rodrigogarciaavila26@gmail.com>
2021-03-18 18:05:55 -0600

    Sidebar done

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
 
-const material_componentes = [
+
+
+const material_components = [
   MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatListModule,
+  MatDividerModule,
 ]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ...material_componentes,
+    ...material_components,
   ],
   exports:[
-    ...material_componentes
+    ...material_components
   ]
 })
 export class MaterialModule { }
