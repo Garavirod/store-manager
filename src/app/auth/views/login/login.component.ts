6910f7b762cec95def47192ad9e3638e2a486620
Garavirod <rodrigogarciaavila26@gmail.com>
2021-04-07 19:53:19 -0500

    reactive form was added to login component

--- src/app/auth/views/login/login.component.ts
+++ src/app/auth/views/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-  hide = true; //hide password
-  constructor() { }
+export class LoginComponent implements OnInit {  
+  formGroup: FormGroup; 
+  constructor( private fb:FormBuilder ) {
+    // Initialize formgroup
+    this.formGroup = this.getFormGroup(); 
+  }
 
   ngOnInit(): void {
+  }
+
+  isValidForm():boolean{
+    return this.formGroup.invalid;
+  }
+
+
+  private getFormGroup():FormGroup{
+    return this.fb.group({
+      email:['',[
+        Validators.required,
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"), 
+      ]],
+      password:['',[
+        Validators.required
+      ]]
+    });
+  }
+
+  /* Login */
+  login(){
+    
   }
 
 }
--- src/app/auth/views/register/register.component.ts
+++ src/app/auth/views/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
